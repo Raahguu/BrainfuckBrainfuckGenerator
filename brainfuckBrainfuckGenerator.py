a0e52f5484cf45b614ffaa43da26beb7945e7ff7
Raahguu <raahguu@outlook.com>
2025-06-10 21:13:58 +0800

    Now has looping to avoid long strings of the same character

--- brainfuckBrainfuckGenerator.py
+++ brainfuckBrainfuckGenerator.py
@@ -33,6 +33,29 @@ for char in sanitised_brainfuck:
         cursor_index -= 1
     new_code += "."
 
+#Add loops, so it istn't just arrows and dots
+repeat_count = 0
+max_repeat_count = 4
+offset = 0
+char_diff = 0
+for i, char in enumerate(new_code):
+    if char == ".":
+        repeat_count += 1
+        continue
+    old_offset = offset
+    if char == ">": offset += 1
+    elif char == "<": offset -= 1
+    if repeat_count <= max_repeat_count:
+        repeat_count = 0
+        continue
+    insertion = "<" * old_offset + "+" * (int(repeat_count ** 0.5)) + "[" + ">" * old_offset + "." * int(repeat_count ** 0.5) + "<" * old_offset + "-]" + ">" * old_offset \
+              + "." * (repeat_count - int(repeat_count ** 0.5) * int(repeat_count ** 0.5))
+    replaced_code = new_code[i-repeat_count:i]
+    new_code = new_code[:i-repeat_count + char_diff] + insertion + new_code[i + char_diff:]
+    char_diff += len(insertion) - len(replaced_code)
+    repeat_count = 0
+
+#Wraps text lines to a cap
 char_count_to_new_line = 50
 for i in range(1, len(new_code)//char_count_to_new_line + 1):
     new_code = new_code[:i * char_count_to_new_line] + "\n" + new_code[i * char_count_to_new_line:]
