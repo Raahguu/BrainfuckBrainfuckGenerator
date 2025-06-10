1f38cc78c9f03aaa869474e55d65473e133da26a
Raahguu <raahguu@outlook.com>
2025-06-10 18:30:58 +0800

    Writes out basic brainfuck code

--- brainfuckBrainfuckGenerator.py
+++ brainfuckBrainfuckGenerator.py
@@ -14,3 +14,19 @@ original_brainfuck = input("Input your Brainfuck: \n")
 
 #Remove unnecessary characters
 sanitised_brainfuck = "".join([char if char in allowed_characters else "" for char in list(original_brainfuck)])
+
+new_code = ""
+#Create the basic code
+for char in sanitised_brainfuck:
+    offset = background_memory_array.index(char) - cursor_index
+    while offset > 0:
+        new_code += ">"
+        offset -= 1
+        cursor_index += 1
+    while offset < 0:
+        new_code += "<"
+        offset += 1
+        cursor_index -= 1
+    new_code += "."
+
+print(f"The new brainfuck code: \n{setup_code}\n{new_code}")
